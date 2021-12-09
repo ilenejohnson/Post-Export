f63e09cdb282762b2e7d297c7a962ee5c70ea61f
ilenejohnson <ilenej@comcast.net>
2021-12-09 10:14:32 -0500

    added readme

--- post-export.php
+++ post-export.php
@@ -1,13 +1,29 @@
 <?php
 /*
 Plugin Name: Post Export
-Plugin URI: https://ikjweb.com/
-Description: Plugin let user select posts to export
+Plugin URI: https://github.com/ilenejohnson/Post-Export
+Description: Plugin to let user select individual posts to export via the post listing
 Version: 1.0
+Requires at least: 5.5
+Requires PHP: 7.2
 Author: Ilene Johnson
 Author URI: https://ikjweb.com
 License: GPLv2 or later
 Text Domain: select_post_export
+
+
+Post Export is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Post Export is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Post Export. If not, see {URI to Plugin License}.
  */
 
 
